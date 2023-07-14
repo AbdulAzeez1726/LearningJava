ae11ca7bc17dfd91aa0fd1f140cec0e1ea1f14e6
AbdulAzeez1726 <mohammadabdulazeez1126@gmail.com>
2023-07-14 18:56:35 +0530

    DataTypes

---
+++ src/com/abd/intro/DataTypes.java
@@ -0,0 +1,21 @@
+package com.abd.intro;
+
+public class DataTypes {
+	public static void main(String args[]) {
+		byte b = 26;
+		int num = 17;
+		short sh = 171;
+		long lg = 6374356;
+		float ft = 6.27f;
+		double db = 2.4432234;
+		char ch = 's';
+		System.out.println(b);
+		System.out.println(num);
+		System.out.println(sh);
+		System.out.println(lg);
+		System.out.println(ft);
+		System.out.println(db);
+		System.out.println(ch);
+
+	}
+}
