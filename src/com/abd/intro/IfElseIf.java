b91981fdd3c3d8ab1eed71e841bff6b32b0cce33
AbdulAzeez1726 <mohammadabdulazeez1126@gmail.com>
2023-07-26 12:35:45 +0530

    If Else If Statement

---
+++ src/com/abd/intro/IfElseIf.java
@@ -0,0 +1,15 @@
+package com.abd.intro;
+
+public class IfElseIf {
+	public static void main(String args []) {
+		int a = 7;
+		int b = 12;
+		int c = 8;
+		if(a>b && a>c)
+			System.out.println(a);
+		else if(b>c)
+			System.out.println(b);
+		else
+			System.out.println(c);
+	}
+}
