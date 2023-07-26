69a31b586355bb7b68d052fa9b1c91ac23715ceb
AbdulAzeez1726 <mohammadabdulazeez1126@gmail.com>
2023-07-26 16:38:51 +0530

    Do While Loop

---
+++ src/com/abd/intro/DoWhile.java
@@ -0,0 +1,11 @@
+package com.abd.intro;
+
+public class DoWhile {
+	public static void main(String args[]) {
+		int a = 5;
+		do {
+			System.out.println("hi " + a);
+			a++;
+		} while (a <= 4);
+	}
+}
