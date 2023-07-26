9471abc6433055c44a01f3162b0d825be5ed5f11
AbdulAzeez1726 <mohammadabdulazeez1126@gmail.com>
2023-07-26 12:04:33 +0530

    If Else Statement

---
+++ src/com/abd/intro/IfElse.java
@@ -0,0 +1,13 @@
+package com.abd.intro;
+
+public class IfElse {
+	public static void main (String args[]) {
+		int a = 27;
+		int b= 16;
+		if(a>b)
+			System.out.println(a);
+		else
+			System.out.println(b);
+	}
+
+}
