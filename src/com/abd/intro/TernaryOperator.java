fb209b829b55295f6546721b702019fb4d1f27fe
AbdulAzeez1726 <mohammadabdulazeez1126@gmail.com>
2023-07-26 12:52:00 +0530

    Ternary Operator

---
+++ src/com/abd/intro/TernaryOperator.java
@@ -0,0 +1,10 @@
+package com.abd.intro;
+
+public class TernaryOperator {
+	public static void main(String args[]) {
+		int a = 26;
+		int b = 0;
+		b = a % 2 == 0 ? 1 : 2;
+		System.out.println(b);
+	}
+}
